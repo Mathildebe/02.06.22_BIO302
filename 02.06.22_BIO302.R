df1f94363099282268fb4ee98afa614db4e259d2
mathildebe <mathildeeikefjord@hotmail.com>
2022-06-02 14:07:58 +0200

    Initial commit

---
+++ 02.06.22_BIO302.R
@@ -0,0 +1,13 @@
+install.packages("usethis")
+library(usethis)
+
+
+library(usethis)
+use_git_config(
+  user.name = "mathildebe", 
+  user.email = "mathildeeikefjord@hotmail.com"
+)
+
+
+usethis::create_github_token()
+  
