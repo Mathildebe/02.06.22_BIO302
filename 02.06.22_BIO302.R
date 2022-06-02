99664ebad946f430b2d4c298ca094e61c08d8a96
mathildebe <mathildeeikefjord@hotmail.com>
2022-06-02 15:07:18 +0200

    Plott lagt inn

--- 02.06.22_BIO302.R
+++ 02.06.22_BIO302.R
@@ -13,5 +13,31 @@ usethis::create_github_token()
 
 gitcreds::gitcreds_set()
 
-# 2.6)  Making a repo
+# 2.6)  Making a repo / oppbevaringssted
 usethis::use_git()
+
+# Create a repo / oppbevaringssted on GitHub
+use_github()
+
+# Endrer navnet frå master til main, ettersom dette no er main branch. 
+git_default_branch_rename()
+
+
+
+
+
+# Laste inn pakker
+library(ggplot2)
+library(palmerpenguins)
+
+palmerpenguins::penguins
+
+# Make a plot 
+ggplot(data = penguins,
+       mapping = aes(x = body_mass_g, y = bill_length_mm, colour = species)) +
+  geom_point() +
+  labs(x = "Body mass, g", y = "Bill length, mm", colour = "Species") +
+  theme_bw()
+
+
+
